4c9dc7bfbc1f6249d7cfefedefcf9a65162d36dc
Blai Samitier <blai@samitier.me>
2016-09-17 02:12:20 +0200

    first layout of the board

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'main-app',
-  template: '<h1>hello world this is angular 2</h1>'
+  template: `
+
+  `
 })
 export class AppComponent { }
